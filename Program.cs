921ae933d56f52b0d7b69404837971c56ad84c84
lusine <lusine.sargsyan71@outlook.com>
2019-09-23 15:06:46 +0400

    lift

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("======lift=====");
+            Console.Write("Where you are heading? ");
+
+            a = Int32.Parse(Console.ReadLine());
+
+            b = 1;
+
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(3000);
+            }
+            Console.Beep();
+            b = a;
+            Console.Write("On which floor are you now? ");
+            a = Int32.Parse(Console.ReadLine());
+            if (b > a)
+            {
+                for (int i = b; i >= a; i--)
+
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(3000);
+                }
+
+
+
+                Console.Beep();
+                b = a;
+
+            }
+            else
+            {
+                for (int i = b; i <= a; i++)
+
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(3000);
+                }
+
+                Console.Beep();
+                b = a;
+            }
+            Console.Write("Where you are heading? ");
+            a = Int32.Parse(Console.ReadLine());
+            if (b > a)
+            {
+                for (int i = b; i >= a; i--)
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(3000);
+                }
+                Console.Beep();
+                b = a;
+            }
+            else
+            {
+                for (int i = b; i <= a; i++)
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(3000);
+                }
+                Console.Beep();
+                b = a;
+            }
+        }
+    }
+}
